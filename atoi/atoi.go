4d873c1adb332d46e3e50f83ad16349d7c12de80
Andrey Lobanov <lobanov728@gmail.com>
2020-10-18 11:00:30 +0300

    inverce binary tree

--- atoi/atoi.go
+++ atoi/atoi.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
 func MyAtoi(s string) int {
 	s = strings.Trim(s, " ")
-	i, err := strconv.Atoi(s)
-	fmt.Println(err)
-	return i
+	//for i, ch := range s {
+	//	fmt.Println(s[i])
+	//	fmt.Println(fmt.Sprintf("%T", s[i]))
+	//	fmt.Println(fmt.Sprintf("%c", ch))
+	//}
+	if s[0] == '-' || s[0] == '+' {
+		s = s[1:]
+	}
+	n := 0
+	for _, ch := range []byte(s) {
+		fmt.Println(ch)
+		fmt.Println(byte('0'))
+		ch -= '0'
+		fmt.Println(ch)
+		//fmt.Println(fmt.Sprintf("%c", ch))
+		//fmt.Println(int(ch))
+		//if ch > 9 {
+		//	return 0, &NumError{fnAtoi, s0, ErrSyntax}
+		//}
+		//n = n*10 + int(ch)
+	}
+	return n
+	//i, err := strconv.Atoi(s)
+	//fmt.Println(err)
+	//if i < math.MinInt32 {
+	//	return math.MinInt32
+	//}
+	//if i > math.MaxInt32 {
+	//	return math.MaxInt32
+	//}
 }
 
 func main() {
--- atoi/atoi_test.go
+++ atoi/atoi_test.go
@@ -5,27 +5,52 @@ import (
 	"testing"
 )
 
-func TestSimple(t *testing.T) {
-	var assert int = 42
+//func TestSimple(t *testing.T) {
+//	var assert int = 42
+//
+//	res := MyAtoi("42")
+//	if !reflect.DeepEqual(assert, res) {
+//		t.Error(res, " not equal ", assert)
+//	}
+//}
+//
+//func TestSimpleWithPlus(t *testing.T) {
+//	var assert int = 42
+//
+//	res := MyAtoi("+42")
+//	if !reflect.DeepEqual(assert, res) {
+//		t.Error(res, " not equal ", assert)
+//	}
+//}
 
-	res := MyAtoi("42")
-	if !reflect.DeepEqual(assert, res) {
-		t.Error(res, " not equal ", assert)
-	}
-}
-
-func TestMinusSimple(t *testing.T) {
-	var assert int = -42
-	res := MyAtoi("   -42")
-	if !reflect.DeepEqual(assert, res) {
-		t.Error(res, " not equal ", assert)
-	}
-}
+//func TestMinusSimple(t *testing.T) {
+//	var assert int = -42
+//	res := MyAtoi("   -42")
+//	if !reflect.DeepEqual(assert, res) {
+//		t.Error(res, " not equal ", assert)
+//	}
+//}
 
 func TestWithWords(t *testing.T) {
 	var assert int = 4193
-	res := MyAtoi("4193 with words")
+	res := MyAtoi("4193 with words что то еще")
 	if !reflect.DeepEqual(assert, res) {
 		t.Error(res, " not equal ", assert)
 	}
 }
+
+//func TestWithWordsInBegin(t *testing.T) {
+//	var assert int = 0
+//	res := MyAtoi("with words что то еще 4193")
+//	if !reflect.DeepEqual(assert, res) {
+//		t.Error(res, " not equal ", assert)
+//	}
+//}
+//
+//func TestOverFlow(t *testing.T) {
+//	var assert int = math.MinInt32
+//	res := MyAtoi("-91283472332")
+//	if !reflect.DeepEqual(assert, res) {
+//		t.Error(res, " not equal ", assert)
+//	}
+//}
